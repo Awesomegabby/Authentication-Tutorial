07d7f66fe2f0853172076d28ae0abcb19639815a
Shemuel Chukwukadibia <shemmy80@gmail.com>
2023-09-30 14:16:09 +0100

    Level 1 - Username and Password Only

---
+++ app.js
@@ -0,0 +1,66 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs")
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+    try {
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+        await newUser.save();
+    } catch (err) {
+        console.log(err);
+    } finally {
+        res.render("secrets");
+    }
+});
+
+app.post("/login", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const foundUsername = await User.findOne(
+        {email: username}
+    ).catch((err) => console.log(err));
+    if (foundUsername) {
+        const foundPassword = await User.findOne(
+            {password: password}
+        ).catch((err) => console.log(err));
+        res.render("secrets");
+    } else {
+        res.send("Wrong login details");
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
